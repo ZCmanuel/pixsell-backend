39853c9ba27aa3f8736f9abbe22d485f21e3051c
Manuel Zamorano Cardell <manuelzamoranodaw@gmail.com>
2025-05-02 19:41:08 +0200

    feat: configuración de JWT

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,16 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model
+
+class Usuario extends Model implements JWTSubject
 {
     protected $table = 'usuarios';
     protected $primaryKey = 'id_usuario';
 
     protected $fillable = ['nombre', 'email', 'contraseña', 'rol'];
 
+    /**
+     * Relación uno a muchos con la tabla Album
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Album, Usuario>
+     */
     public function albumes()
     {
         return $this->hasMany(Album::class, 'id_usuario');
+    }
+
+
+    /**
+     *  Metoodo de JWT para obtener el identificador del usuario 
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Método de JWT para obtener las reclamaciones personalizadas
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
